os-autoinst/openqa_review

commit 7e2e64dc713e245f77bbda2724b91532bf4b07b0

Improve handling broken softfailures
* Remove uncovered and unused code branch to handle an invalid bug tracker
  URL
    * This case should never occur because we can only find bug references
      of known bug trackers anyways
* Avoid duplicating logic for "recomposing" bug reference after finding
  a match
    * Always use a hash, e.g. `bsc#123` and never just `bsc123`
    * Affects the (alphabetical) ordering (see fixtures changes)
* Use "missing/unsupported bug reference" as error indication because we
  cannot distinguish whether a bug reference is missing or whether we the
  format is just not supported
* Remove uncovered code branch logging "Could not find bug reference in …"
  unifying it with the "missing/unsupported bug reference" case mentioned
  in the previous point
* Remove uncovered code branch for skipping if there's no match unifying it
  with the "missing/unsupported bug reference" case
* Make it explicit in the code that we have one case where we do not
  actually require a valid bug reference (when a text result starts with
  `Soft Failure:` we just pass the subsequent text as in test 684839 of the
  fixtures)
* See https://progress.opensuse.org/issues/95715#note-16

openqa_review/openqa_review.py
@@ -942,9 +942,7 @@ class ArchReport(object):
                     module_url = self._get_url_to_softfailed_module(v["href"])
                     module_name = re.search("[^/]*/[0-9]*/[^/]*/([^/]*)/[^/]*/[0-9]*", module_url).group(1)
                     assert module_name, "could not find a module name within %s in job %s" % (module_url, v["href"])
-                    v["bugref"] = self._get_bugref_for_softfailed_module(v, module_name)
-                    if not v["bugref"]:  # pragma: no cover
-                        continue
+                    match, found_actual_ref = self._get_bugref_for_softfailed_module(v, module_name)
                 except AttributeError:  # pragma: no cover
                     log.info(
                         "Could find neither soft failed info box nor needle, assuming an old openQA job, skipping."
@@ -953,21 +951,14 @@ class ArchReport(object):
                 except DownloadError as e:  # pragma: no cover
                     log.error("Failed to process %s with error %s. Skipping current result" % (v, e))
                     continue
-                match = re.search(bugref_regex, v["bugref"])
-                if not match:  # pragma: no cover
-                    log.info("Could not find bug reference in text '%s', skipping." % v["bugref"])
+                if not found_actual_ref:
+                    v["bugref"] = match
                     continue
-                v["bugref"] = match.group(0)
-                bugref, bug_id = match.group(1), match.group(2)
-                assert bugref, "No bugref found for %s" % v
+                bug_tracker, bug_id = match.group(1), match.group(2)
+                assert bug_tracker, "No bugref found for %s" % v
                 assert bug_id, "No bug_id found for %s" % v
-                try:
-                    v["bugref_href"] = issue_tracker[bugref](bug_id)
-                except KeyError as e:  # pragma: no cover
-                    log.error(
-                        "Failed to find valid bug tracker URL for %s with error %s. Skipping current result" % (v, e)
-                    )
-                    continue
+                v["bugref"] = "%s#%s" % (bug_tracker, bug_id)
+                v["bugref_href"] = issue_tracker[bug_tracker](bug_id)
 
     @property
     def total_issues(self):
@@ -1002,6 +993,7 @@ class ArchReport(object):
         log.debug("Retrieving '%s'" % details_url)
         details_json = self.test_browser.get_json(details_url)
         details = details_json["details"] if "details" in details_json else details_json
+        match = None
         for field in details:
             if "title" in field and "Soft Fail" in field["title"]:
                 if "text_data" in field:
@@ -1010,11 +1002,14 @@ class ArchReport(object):
                     unformated_str = self.test_browser.get_soup(
                         "%s/file/%s" % (rel_job_url, quote(field["text"]))
                     ).getText()
-                return re.search("Soft Failure:\n(.*)", unformated_str.strip()).group(1)
+                match = re.search(bugref_regex, unformated_str)
+                if not match:  # use the "Soft Failure: …" text as bugref instead
+                    match = re.search("Soft Failure:\n(.*)", unformated_str.strip())
+                    if match:
+                        return (match.group(1), False)
             # custom results can have soft-fail as well
             if "result" in field and "softfail" in field["result"]:
                 match = re.search(bugref_regex, field["title"])
-                return match.group(1) + "#" + match.group(2)
             elif "properties" in field and len(field["properties"]) > 0 and field["properties"][0] == "workaround":
                 log.debug("Evaluating potential workaround needle '%s'" % field["needle"])
                 match = re.search(bugref_regex, field["needle"])
@@ -1024,9 +1019,9 @@ class ArchReport(object):
                         % rel_job_url
                     )
                     continue
-                return match.group(1) + "#" + match.group(2)
-        else:  # pragma: no cover
-            return "missing bug reference"
+            if match:
+                return (match, True)
+        return ("missing/unsupported bug reference", False)
 
     def has_todo_issues(self):
         """Tell if report has new or existing todo issues."""

tests/broken-softfails/:api:v1:jobs:5590739:details
@@ -2,7 +2,7 @@
    "job" : {
       "name" : "sle-11-SP3-Server-DVD-Incidents-Kernel-x86_64-Build:18520:kernel-ec2-ltp_containers@64bit",
       "result" : "softfailed",
-      "test" : "ltp_containers_1",
-      "testresults" : [{ "details" : [], "name" : "netns_sysfs", "result" : "softfailed" }]
+      "test" : "ltp_containers_2",
+      "testresults" : [{ "details" : [], "name" : "yet-another-detail", "result" : "softfailed" }]
    }
 }

tests/broken-softfails/:tests:overview%3Fdistri%3Dsle%26version%3D11-SP3%26build%3D%253A18520%253Akernel-ec2%26groupid%3D161
@@ -9,10 +9,18 @@
     <thead><tr><th>Test</th><th id="flavor_Server-DVD-Incidents-Kernel_arch_x86_64">x86_64</th></tr></thead>
     <tbody>
         <tr>
-            <td class="name"><a data-title="ltp_containers"><span title="ltp_containers">ltp_containers</span></a></td>
-            <td id="res_Server-DVD-Incidents-Kernel_x86_64_ltp_containers" name="jobid_td_5590738">
+            <td class="name"><a data-title="ltp_containers_1"><span title="ltp_containers_1">ltp_containers</span></a></td>
+            <td id="res_Server-DVD-Incidents-Kernel_x86_64_ltp_containers_1" name="jobid_td_5590738">
                 <span id="res-5590738">
                     <a href="/tests/5590738"><i class="status fa fa-circle result_softfailed" title="Done: softfailed"></i></a>
+                </span>
+            </td>
+        </tr>
+        <tr>
+            <td class="name"><a data-title="ltp_containers_2"><span title="ltp_containers_2">ltp_containers</span></a></td>
+            <td id="res_Server-DVD-Incidents-Kernel_x86_64_ltp_containers_2" name="jobid_td_5590738">
+                <span id="res-5590739">
+                    <a href="/tests/5590739"><i class="status fa fa-circle result_softfailed" title="Done: softfailed"></i></a>
                 </span>
             </td>
         </tr>

commit 8abe0fc9b9960dc39f6e738b727b59af85fcb744

Avoid further search for softfails if title contains "Soft Fail"
This fixes an unintended change of the behavior introduced in
7e2e64dc713e245f77bbda2724b91532bf4b07b0 which removed the early return
when returning a bugref found within a "Soft Fail" title. This change
fixes that by using `elif` for the next condition.

openqa_review/openqa_review.py
@@ -1008,7 +1008,7 @@ class ArchReport(object):
                     if match:
                         return (match.group(1), False)
             # custom results can have soft-fail as well
-            if "result" in field and "softfail" in field["result"]:
+            elif "result" in field and "softfail" in field["result"]:
                 match = re.search(bugref_regex, field["title"])
             elif "properties" in field and len(field["properties"]) > 0 and field["properties"][0] == "workaround":
                 log.debug("Evaluating potential workaround needle '%s'" % field["needle"])